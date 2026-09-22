5445cc29e5ff84a33b88fdfccf53fcacc37d5b60
Juan Camilo <jk.ramirezsanchez@gmail.com>
2026-09-21 20:19:00 -0500

    Se implemento mecanica del personaje, inteligencia personal

--- Scripts/Enemies/trap.gd
+++ Scripts/Enemies/trap.gd
@@ -1,15 +1,33 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var speed = 25
+@export var speed = 200  
+@export var margin = 8  
 
 var player: Node2D
+var start_pos: Vector2
+var target_pos: Vector2
+var going = false       
 
 func _ready():
+	start_pos = position
 	player = get_node("../Player")
 
 func _process(delta):
 	if player == null:
 		return
 
-	var direction = (player.position - position).normalized()
-	position += direction * speed * delta
+	var diff = player.position - start_pos
+
+	var aligned = abs(diff.x) < margin or abs(diff.y) < margin
+
+	if aligned and diff != Vector2.ZERO:
+		going = true
+		if abs(diff.x) < margin:
+			target_pos = Vector2(start_pos.x, player.position.y)
+		else:
+			target_pos = Vector2(player.position.x, start_pos.y)
+	else:
+		going = false
+		target_pos = start_pos
+
+	position = position.move_toward(target_pos, speed * delta)
