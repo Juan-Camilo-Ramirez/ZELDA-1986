808b69bc0a113edccb837ab4aa54a5a635e769a1
Juan Camilo <jk.ramirezsanchez@gmail.com>
2026-09-21 20:37:36 -0500

    Mecanica de movimiento

--- Scripts/Enemies/trap.gd
+++ Scripts/Enemies/trap.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 200  
+@export var speed = 120  
+@export var return_speed = 40   
 @export var margin = 8  
 
 var player: Node2D
 var start_pos: Vector2
 var target_pos: Vector2
-var going = false       
 
 func _ready():
 	start_pos = position
@@ -17,17 +17,16 @@ func _process(delta):
 		return
 
 	var diff = player.position - start_pos
-
 	var aligned = abs(diff.x) < margin or abs(diff.y) < margin
+	var current_speed = speed
 
 	if aligned and diff != Vector2.ZERO:
-		going = true
 		if abs(diff.x) < margin:
 			target_pos = Vector2(start_pos.x, player.position.y)
 		else:
 			target_pos = Vector2(player.position.x, start_pos.y)
 	else:
-		going = false
 		target_pos = start_pos
+		current_speed = return_speed
 
-	position = position.move_toward(target_pos, speed * delta)
+	position = position.move_toward(target_pos, current_speed * delta)
